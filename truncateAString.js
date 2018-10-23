c8c0084d5ee692d01bee26ccfd56cb9bc0bc9557
Braden Shipley <bradenrshipley@gmail.com>
2018-10-23 08:07:46 -0500

    Add files via upload

---
+++ truncateAString.js
@@ -0,0 +1,17 @@
+﻿<script type="text/javascript">
+/*
+Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
+*/
+function truncateString(str, num) {
+  
+if (str.length>num && num > 3){
+    return str.slice(0,(num))+'...';
+    } else if (str.length>num && num <= 3){
+        return str.slice(0,num) +'...';
+        } else {
+          return str;
+          }
+};
+
+
+</script>
