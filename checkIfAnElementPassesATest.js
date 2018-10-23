005170ce7f1279f56f1f831c42249c9bcb0c5459
Braden Shipley <bradenrshipley@gmail.com>
2018-10-23 08:12:55 -0500

    Add files via upload

---
+++ checkIfAnElementPassesATest.js
@@ -0,0 +1,16 @@
+﻿<script type="text/javascript">
+/*
+Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument). If no element passes the test, return undefined.
+*/
+function findElement(arr, func) {
+  let num = 0;
+  for (let i=0;i<arr.length;i++){
+    let num=arr[i];
+    if (func(num)){
+      return num;
+    }
+  }
+  return undefined;
+}
+
+</script>
